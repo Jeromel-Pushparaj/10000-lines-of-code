aca25f7b0f45f253e6afea1c723daf371ad6073d
Jeromel Pushparaj <116452699+Jeormel@users.noreply.github.com>
2023-11-08 07:36:00 +0530

    try to solve new problem

---
+++ problems/lognnameinarray.c
@@ -0,0 +1,32 @@
+/*
+Given a list of names, display the longest name.
+
+
+Example:
+
+Input:
+N = 5
+names[] = { "Geek", "Geeks", "Geeksfor",
+  "GeeksforGeek", "GeeksforGeeks" }
+
+Output:
+GeeksforGeeks
+ 
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function longest() which takes the array names[] and its size N as inputs and returns the Longest name in the list of names.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+*/
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main(){
+    int N = 5;
+    char names[N][30] = { "Geek", "Geeks", "Geeksfor","GeeksforGeek", "GeeksforGeeks" };
+    for(int i=0;i<N;i++){
+        
+    }
+}
