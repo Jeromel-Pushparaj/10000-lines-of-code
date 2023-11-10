eaa51f18f73480adb41cce496091aedcd61b09ba
Jeromel Pushparaj <116452699+Jeormel@users.noreply.github.com>
2023-11-10 21:19:49 +0530

    try to solve the finding large string in array using c programming

--- problems/lognnameinarray.c
+++ problems/lognnameinarray.c
@@ -23,10 +23,21 @@ Expected Auxiliary Space: O(1)
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+char large(char a[30], char b[30]){
+  int al = strlen(a);
+  int bl = strlen(b);
+  if(al>bl)
+    return a[30];
+  else
+    return b[30];
+}
 int main(){
     int N = 5;
     char names[N][30] = { "Geek", "Geeks", "Geeksfor","GeeksforGeek", "GeeksforGeeks" };
     for(int i=0;i<N;i++){
-        
+        char a[30]=name[i];
+        char b[30]=name[i+1];
+        char c[30]=large(a,b);
     }
+    printf("%s",c);
 }
