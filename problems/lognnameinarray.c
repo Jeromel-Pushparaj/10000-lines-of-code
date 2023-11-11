237570321dfbca123306c42e4d97360523ca721d
Jeromel Pushparaj <116452699+Jeormel@users.noreply.github.com>
2023-11-11 20:15:19 +0530

    largest among list is solved

--- problems/lognnameinarray.c
+++ problems/lognnameinarray.c
@@ -20,24 +20,35 @@ You don't need to read input or print anything. Your task is to complete the fun
 Expected Time Complexity: O(N)
 Expected Auxiliary Space: O(1)
 */
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-char large(char a[30], char b[30]){
-  int al = strlen(a);
-  int bl = strlen(b);
-  if(al>bl)
-    return a[30];
-  else
-    return b[30];
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function to find the larger string between two strings
+char* large(char* str1, char* str2) {
+    return strlen(str1) > strlen(str2) ? str1 : str2;
 }
-int main(){
+
+int main() {
     int N = 5;
-    char names[N][30] = { "Geek", "Geeks", "Geeksfor","GeeksforGeek", "GeeksforGeeks" };
-    for(int i=0;i<N;i++){
-        char a[30]=name[i];
-        char b[30]=name[i+1];
-        char c[30]=large(a,b);
+    char** names = (char**)malloc(N * sizeof(char*));
+    names[0] = "Geekgeekgeekgeekgeek";
+    names[1] = "Geeks";
+    names[2] = "Geeksforffssffddssdsgeeksls";
+    names[3] = "GeeksforGeek";
+    names[4] = "GeeksforGeeks";
+
+    // Initialize to an empty string, assuming there is at least one non-empty string in the array
+    char* largest = names[0];
+
+    int i = 0;
+while (i < N) {
+        char* b = names[i];
+        largest = large(largest, b);
+        i++;
     }
-    printf("%s",c);
+
+    printf("%s", largest);
+
+    return 0;
 }
