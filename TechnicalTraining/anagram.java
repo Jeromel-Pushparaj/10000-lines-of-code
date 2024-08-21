81944911ab63493f8c90624678542955123faf09
Jeromel Pushparaj <jeromal03@gmail.com>
2024-08-21 13:49:40 +0530

    morning section2 problems

---
+++ TechnicalTraining/anagram.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class anagram {
+    public static void main(String[] args) {
+        String s1 = "care";
+        String s2 = "race";
+
+        char c1[] =  s1.toCharArray();
+        char c2[] =  s2.toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        boolean bol = Arrays.equals(c1,c2);
+        System.out.println(bol);
+
+    }
+}
---
+++ TechnicalTraining/bubblesort.java
@@ -0,0 +1,14 @@
+public class bubblesort {
+    public static void main(String[] args) {
+        String s=" Welcom to the java Trainning cal";
+        int count = 0;
+        s=s.trim();
+        int len = s.length();
+        for(int i=0; i<len;i++){
+            if(s.charAt(i) == ' '){
+                count++;
+            }
+        }
+        System.out.println(count+1);
+    }
+}
