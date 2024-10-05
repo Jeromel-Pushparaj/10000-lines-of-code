04f5ad444aa4eba2aea91ce376451b7b1abd4f80
Jeromel Pushparaj <jeromal03@gmail.com>
2024-10-05 15:42:47 +0530

    Today I did Task mangement for sass

--- Learning_Grunt/projects/grunt/Gruntfile.js
+++ Learning_Grunt/projects/grunt/Gruntfile.js
@@ -1,119 +1,158 @@
 module.exports = function(grunt){
-    // Project configuration.
-    grunt.initConfig({
-        concat: {
-        options: {
-            separator: '\n',
-            sourceMap: true,
-            banner: "/*Writen by Jeromel*/\n"
-        },
-        css: {
-            src: [
-                '../css/**/*.css',
-            ],
-            dest: 'dist/style.css',
-        },
-        js: {
-          src: [
-              '../js/**/*.js',
-          ],
-          dest: 'dist/app.js',
+  // Project configuration.
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: '\n',
+        sourceMap: true,
+        banner: "/*Writen by Jeromel*/\n"
       },
+      css: {
+        src: [
+          '../css/**/*.css',
+        ],
+        dest: 'dist/style.css',
+      },
+      scss: {
+        src: [
+          '../scss/**/*.scss',
+        ],
+        dest: 'dist/style.scss',
+      },
+      js: {
+        src: [
+          '../js/**/*.js',
+        ],
+        dest: 'dist/app.js',
+      },
+    },
+    
+    
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      css: {
+        files: {
+          '../../htdocs/css/style.min.css': ['dist/style.css']
+        }
+      },
+      scss: {
+        files: {
+          '../../htdocs/css/app.css': ['../../htdocs/css/app.css']
+        }
+      }
+    },
+    
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
         },
-
-
-        cssmin: {
-          options: {
-            mergeIntoShorthands: false,
-            roundingPrecision: -1
+        files: {                         // Dictionary of files
+          '../../htdocs/css/app.css': 'dist/style.scss',       // 'destination': 'source'
+        }
+      }
+    },
+    
+    
+    copy: {
+      main: {
+        files: [
+          // includes files within path
+          {
+            expand: true,
+            flatten: true,
+            filter: 'isFile',
+            src: ['bower_components/jquery/dist/*'], 
+            dest: '../../htdocs/js/jquery/', 
+            
           },
-          target: {
-            files: {
-              '../../htdocs/css/style.min.css': ['dist/style.css']
-            }
-          }
+          
+        ],
+      },
+    },
+    obfuscator: {
+      options: {
+        banner: '// obfuscated with grunt-contrib-obfuscator.\n',
+        debugProtection: true,
+        debugProtectionInterval: true,
+        domainLock: ['jeromel.grunt.com']
+      },
+      task1: {
+        options: {
+          // options for each sub task
         },
-
-        copy: {
-          main: {
-            files: [
-              // includes files within path
-              {
-              expand: true,
-              flatten: true,
-              filter: 'isFile',
-              src: ['bower_components/jquery/dist/*'], 
-              dest: '../../htdocs/js/jquery/', 
-              
-            },
-        
-            ],
-          },
+        files: {
+          '../../htdocs/js/app.o.js': [
+            'dist/app.js'
+          ]
+        }
+      }
+    },
+    
+    uglify: {
+      my_target: {
+        options: {
+          sourceMap: true,
         },
-        obfuscator: {
-          options: {
-              banner: '// obfuscated with grunt-contrib-obfuscator.\n',
-              debugProtection: true,
-              debugProtectionInterval: true,
-              domainLock: ['jeromel.grunt.com']
-          },
-          task1: {
-              options: {
-                  // options for each sub task
-              },
-              files: {
-                  '../../htdocs/js/app.o.js': [
-                      'dist/app.js'
-                  ]
-              }
-          }
+        files: {
+          '../../htdocs/js/app.min.js': ['dist/app.js']
+        }
+      }
+    },
+    
+    watch: {
+      css: {
+        files: [
+          '../css/**/*.css',
+        ],//"/**/*.css" - this means what ever file changed in that folder it runs the tasks
+        tasks: [
+          'concat:css',
+          'cssmin:css'
+        ],
+        options: {
+          spawn: true,
+          livereload: true
         },
-
-        uglify: {
-          my_target: {
-            options: {
-              sourceMap: true,
-            },
-            files: {
-              '../../htdocs/js/app.min.js': ['dist/app.js']
-            }
-          }
+      },
+      scss: {
+        files: [
+          '../scss/**/*.scss',
+        ],//"/**/*.css" - this means what ever file changed in that folder it runs the tasks
+        tasks: [
+          'concat:scss',
+          'sass',
+          'cssmin:scss',
+          
+        ],
+        options: {
+          spawn: true,
+          livereload: true
         },
-
-        watch: {
-            css: {
-              files: [
-                '../css/**/*.css',
-              ],//"/**/*.css" - this means what ever file changed in that folder it runs the tasks
-              tasks: [
-                'concat:css',
-                'cssmin'
-              ],
-              options: {
-                spawn: true,
-                livereload: true
-              },
-            },
-              js: {
-                files: [
-                  '../js/**/*.js',
-                ],//"/**/*.css" - this means what ever file changed in that folder it runs the tasks
-                tasks: ['concat:js', 'uglify', 'obfuscator'],
-                options: {
-                  spawn: true,
-                  livereload: true
-                },
-              },
-          },
-    });
-
-    grunt.loadNpmTasks('grunt-contrib-obfuscator');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.registerTask('css', ['concat:cs', 'cssmin']);
-    grunt.registerTask('js', ['concat:js', 'uglify', 'obfuscator']);
-    grunt.registerTask('default', ['copy','concat','cssmin','uglify','obfuscator','watch']);
+      },
+      js: {
+        files: [
+          '../js/**/*.js',
+        ],//"/**/*.css" - this means what ever file changed in that folder it runs the tasks
+        tasks: ['concat:js', 'uglify', 'obfuscator'],
+        options: {
+          spawn: true,
+          livereload: true
+        },
+      },
+    },
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-obfuscator');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('css', ['concat:cs', 'cssmin', 'sass']);
+  grunt.registerTask('js', ['concat:js', 'uglify', 'obfuscator']);
+  grunt.registerTask('default', ['copy','concat','cssmin:css','sass','cssmin:scss','uglify','obfuscator','watch']);
 }
