dc992203f4a95fea1ca5264d1996402995d1714b
Jeromel Pushparaj <jeromal03@gmail.com>
2024-12-19 17:03:51 +0530

    Blind coding first implementation

---
+++ Learning_java/DSA/LinkedList/LinkedList.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class LinkedList{
+    Node head;
+    class Node{
+        int data;
+        Node next;
+
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+
+    public void insert_at_the_end(int element){
+        Node newNode = new Node(element);
+        if(head == null){
+            head = new Node(element);
+            return;
+        }
+        newNode.next = null;
+        Node last = head;
+
+        while(last.next != null){
+            last = last.next;
+        }
+        last.next = newNode;
+        return;
+
+
+
+    }
+
+    public void printlist(){
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    public static void main(String[] args){
+        LinkedList l = new LinkedList();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i, element;
+        for(i = 1; i<=n;i++){
+            element = sc.nextInt();
+            l.insert_at_the_end(element);
+
+        }
+        l.printlist();
+    }
+}
