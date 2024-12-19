5d1ff4b7ce01640308f37e8e4f522614259f0278
jeromel-pushparaj <jeromal03@gmail.com>
2024-12-19 19:25:45 +0530

    Tomorrow is TOC exam here I am exploring NVIM lol

--- Learning_java/DSA/LinkedList/LinkedList.java
+++ Learning_java/DSA/LinkedList/LinkedList.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 class LinkedList{
+
     Node head;
     class Node{
         int data;
@@ -13,22 +14,22 @@ class LinkedList{
     }
 
     public void insert_at_the_end(int element){
+
         Node newNode = new Node(element);
         if(head == null){
             head = new Node(element);
             return;
         }
+
         newNode.next = null;
         Node last = head;
 
         while(last.next != null){
             last = last.next;
         }
+
         last.next = newNode;
         return;
-
-
-
     }
 
     public void printlist(){
@@ -46,8 +47,9 @@ class LinkedList{
         for(i = 1; i<=n;i++){
             element = sc.nextInt();
             l.insert_at_the_end(element);
-
         }
+
         l.printlist();
+
     }
 }
